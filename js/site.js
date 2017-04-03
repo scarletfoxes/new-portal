540c8b0bebd660686bd26f95a95a2d910c6c4076
EBaxter1 <ebaxter1@hawk.iit.edu>
2017-04-03 15:40:54 -0500

    HTML:fixed it so that type of password input is password JS: added some js to test if un and password are correct and then goes to main page

--- js/site.js
+++ js/site.js
@@ -5,4 +5,18 @@ $.noConflict();
 jQuery(function($){
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
-});
+
+    $('#un').on('keyup focus blur', function(){
+    var currentValue = $(this).val();
+    var vaildPattern = $(this).attr('pattern');
+    console.log(currentValue, vaildPattern);
+    if(currentValue.match(vaildPattern)){
+     $('#pass').on('keyup focus blur', function(){
+    var currentValue = $(this).val();
+    var vaildPattern = $(this).attr('pattern');
+    console.log(currentValue, vaildPattern);
+    if(currentValue.match(vaildPattern)){
+      window.location.href = "/main-page";
+    }
+    }
+  });
