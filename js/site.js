35486441c17b651451b36481773e5fa2dabd4b08
EBaxter1 <ebaxter1@hawk.iit.edu>
2017-04-03 22:13:41 -0500

    Finally made js work so that it checks values of inputs and also upon hitting login takes user to next page Also added for forgot username and password takes user to OTS page

--- js/site.js
+++ js/site.js
@@ -6,56 +6,28 @@ jQuery(function($){
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 
-var currentValue = $(this).val();
-var vaildUser = $(this).attr("jdoe2");
-var vaildPassword = $(this).attr("HAWK2017")
+var currentUser = $('#un').val();
+var vaildUser = "jdoe2";
+var currentPassword = $('#pass').val();
+var vaildPassword = "HAWK2017";
 
-// function validateform(){
-
-// if (user==null || user==""){
-//   alert("Username can't be blank");
-//   return false;
-// }else if(user!="jdoe2"){
-// 	alert("Username not found");
-// 	return false
-// }
-// if(password!="HAWK2017"){
-//   alert("Password/username combination not found");
-//   return false;
-//   }
-// }
-// function (){
-// 	if ($('#pass').match('HAWK2017') && $('#un').match('jdoe2')) {
-// 		window.location.href = "/main-page/";
-// 	}
-// }
  $('#form').on('submit', function (event) {
- if (currentValue.match(vaildUser)) {
-		if (currentValue.match(vaildPassword)) {
+ 	event.preventDefault();
+ if (currentUser=vaildUser) {
+		if (currentPassword=vaildPassword) {
 			var newURL = window.location.href + "main-page";
-			alert(newURL);
- 	 window.location.replace(newURL);
-   		}
+ 	 window.location.href = (newURL);
+   		}	
    else{
   	alert("Username/password combination not found");
    }	
 }
+else{
+	alert("Username not found")
+}
 });
-//   } else {
-//   	$('#un').setCustomValidity("Username not found");
-//   }
-// });
-    // $('#un').on('keyup focus blur', function(){
-    // var currentValue = $(this).val();
-    // var vaildPattern = $(this).attr('pattern');
-    // console.log(currentValue, vaildPattern);
-    // if(currentValue.match(vaildPattern)){
-    //  $('#pass').on('keyup focus blur', function(){
-    // var currentValue = $(this).val();
-    // var vaildPattern = $(this).attr('pattern');
-    // console.log(currentValue, vaildPattern);
-    // if(currentValue.match(vaildPattern)){
-    //   window.location.href = "/main-page";
-    // }
-    // }
+
+$('.forgot').on('click', function (event) {
+ 	 window.location.href = "https://ots.iit.edu/help-and-support";
+});
 });
