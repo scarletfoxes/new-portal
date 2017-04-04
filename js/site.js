6e67cd2cdce7ab79dfa20f9ee9e8529955d52bab
EBaxter1 <ebaxter1@hawk.iit.edu>
2017-04-03 20:32:02 -0500

    Working with js to try and make sure form is vaild Commiting it to test on pages

--- js/site.js
+++ js/site.js
@@ -5,4 +5,51 @@ $.noConflict();
 jQuery(function($){
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
+
+var currentValue = $(this).val();
+var vaildUser = $(this).attr("jdoe2");
+var vaildPassword = $(this).attr("HAWK2017")
+
+// function validateform(){
+
+// if (user==null || user==""){
+//   alert("Username can't be blank");
+//   return false;
+// }else if(user!="jdoe2"){
+// 	alert("Username not found");
+// 	return false
+// }
+// if(password!="HAWK2017"){
+//   alert("Password/username combination not found");
+//   return false;
+//   }
+// }
+
+$('#un').addEventListener($('#submit'), function (event) {
+  if (currentValue.match(vaildUser)) {
+  	$('#pass').addEventListener($('#submit'), function (event) {
+  		if (currentValue.match(vaildPassword)) {
+   	 window.location.href = "/main-page";
+   		}
+    else{
+    	$('#pass').innerHTML("Username/password combination not found");}
+    }
+	});
+  } else {
+  	$('#un').setCustomValidity("Username not found");
+  }
+});
+    // $('#un').on('keyup focus blur', function(){
+    // var currentValue = $(this).val();
+    // var vaildPattern = $(this).attr('pattern');
+    // console.log(currentValue, vaildPattern);
+    // if(currentValue.match(vaildPattern)){
+    //  $('#pass').on('keyup focus blur', function(){
+    // var currentValue = $(this).val();
+    // var vaildPattern = $(this).attr('pattern');
+    // console.log(currentValue, vaildPattern);
+    // if(currentValue.match(vaildPattern)){
+    //   window.location.href = "/main-page";
+    // }
+    // }
 });
